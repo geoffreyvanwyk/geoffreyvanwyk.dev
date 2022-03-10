810bd374fa4d00e1ed93249e16e513fa646f4bac
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2022-03-11 01:17:22 +0200

    style: add margin to tag component

--- components/Tag.vue
+++ components/Tag.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="px-3 py-1 text-xs font-medium text-gray-800 bg-gray-100 border border-gray-100 rounded-full"
+    class="px-3 py-1 my-1 ml-1 text-xs font-medium text-gray-800 bg-gray-100 border border-gray-100 rounded-full"
     >{{ name }}</span
   >
 </template>
--- components/TagList.vue
+++ components/TagList.vue
@@ -1,7 +1,7 @@
 <template>
-  <ul class="flex flex-wrap flex-grow justify-start pl-0 list-none">
-    <li v-for="tag in tags" :key="tag"><Tag :name="tag" /></li>
-  </ul>
+  <div class="flex flex-wrap justify-start pl-0 list-none">
+    <Tag v-for="tag in tags" :key="tag" :name="tag" />
+  </div>
 </template>
 
 <script lang="ts">
