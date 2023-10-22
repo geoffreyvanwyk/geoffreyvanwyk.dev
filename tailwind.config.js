b53c814092815d3a92b2d7a975b1e17b016c830a
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2023-10-22 18:22:42 +0200

    feat: Style main navigation bar using Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./themes/windy/assets/**/*.css', './themes/windy/templates/**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
