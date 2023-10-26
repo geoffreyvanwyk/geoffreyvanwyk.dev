0508c3e4b0a0a5f0067294859e3afcea9861be19
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2023-10-26 16:24:22 +0200

    style: Build Tailwind CSS the regular way

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./themes/windy/assets/**/*.css', './themes/windy/templates/**/*.html'],
+  content: ['./output/**/*.html', './theme/windy/assets/**/*.css'],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography'),],
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-hero-patterns')],
 }
+
