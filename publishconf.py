aa6b91ecd154b2d8e4da396ed50caf60aeb4ca23
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2023-10-31 17:12:25 +0200

    fix: Deployed to subdirectory

--- publishconf.py
+++ publishconf.py
@@ -10,6 +10,7 @@ from pelicanconf import *
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://geoffreyvanwyk.dev"
 RELATIVE_URLS = False
+GITHUB_URL = "https://github.com/geoffreyvanwyk"
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
